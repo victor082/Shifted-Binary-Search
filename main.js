6b9aafdf5f9b606274982c8fb3c532154bf7eae3
victor082 <46913219+victor082@users.noreply.github.com>
2020-01-15 11:35:35 -0500

    Deleted unnecessary console logs

--- main.js
+++ main.js
@@ -11,36 +11,21 @@ function shifted_binary_search(arr,target){
   let temp = []
   let temp2 = []
   for(i= 0; i < arr.length; i++){
-    // console.log('i',arr[i])
-    // console.log('i + 1',arr[i+1])
     if(arr[i] > arr[i + 1] ){
       dropoff = i + 1
       temp = arr.slice(dropoff,arr.length)
       temp2 = arr.slice(0,dropoff)
-      // arr2.push(temp)
-      // console.log('temp',temp)
-      // console.log('temp2',temp2)
-      // console.log('dropoff',dropoff)
-      // console.log('arr2',arr2)
     }
   }
   if(temp2[0] <= target <= temp2[temp2.length - 1]){
     for(i = 0; i< temp2.length; i++){
-      // console.log(i)
-      // console.log(temp2[i])
       if(temp2[i] == target){
-        // console.log(i)
-        // console.log(temp[i])
         return i
       }
     }
   }if(temp[0] <= target <= temp[temp2.length - 1]){
     for(i = 0; i< temp.length; i++){
-      // console.log(i)
-      // console.log(temp2[i])
       if(temp[i] === target){
-        // console.log(i)
-        // console.log(temp[i])
         return i + temp2.length
       }
   }
