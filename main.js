9a828669f53598d451a6a2cb55d778abf261909d
victor082 <46913219+victor082@users.noreply.github.com>
2020-01-15 11:37:18 -0500

    changed temporary names to make more sense

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 function shifted_binary_search(arr,target){
+  
+  //   PSEUDO CODE
   // how might we figure out the rotation index?
   // split up the problem, depending on the number at the rotation index
   // find the point where the values drop off 
@@ -6,28 +8,30 @@ function shifted_binary_search(arr,target){
   // we have two sorted halves we can run two seeparate binary searces on each sorted half
   // find the rotation point
   // run two seeparate binary searches on each half
-  let dropoff = 0
-  let arr2 = []
-  let temp = []
-  let temp2 = []
-  for(i= 0; i < arr.length; i++){
-    if(arr[i] > arr[i + 1] ){
-      dropoff = i + 1
-      temp = arr.slice(dropoff,arr.length)
-      temp2 = arr.slice(0,dropoff)
+  
+let dropoff = 0
+let arr2 = []
+let firstArray = []
+let secondArray = []
+
+for (i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[i + 1]) {
+        dropoff = i + 1
+        secondArray = arr.slice(dropoff, arr.length)
+        firstArray = arr.slice(0, dropoff)
     }
-  }
-  if(temp2[0] <= target <= temp2[temp2.length - 1]){
-    for(i = 0; i< temp2.length; i++){
-      if(temp2[i] == target){
-        return i
-      }
+}
+if (firstArray[0] <= target <= firstArray[firstArray.length - 1]) {
+    for (i = 0; i < firstArray.length; i++) {
+        if (firstArray[i] == target) {
+            return i
+        }
     }
-  }if(temp[0] <= target <= temp[temp2.length - 1]){
-    for(i = 0; i< temp.length; i++){
-      if(temp[i] === target){
-        return i + temp2.length
-      }
-  }
-  return -1
+} if (secondArray[0] <= target <= secondArray[firstArray.length - 1]) {
+    for (i = 0; i < secondArray.length; i++) {
+        if (secondArray[i] === target) {
+            return i + firstArray.length
+        }
+    }
+    return -1
 }
